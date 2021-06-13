f06df30de2371f81d8272334b70bb7cafb71f0d1
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-06-13 19:24:53 +0000

    Complete Pseudocode assignment examples

---
+++ pseudocode.rb
@@ -0,0 +1,51 @@
+# For example, write out pseudo-code (both casual and formal) that does the following:
+# No need to code here, just practice writing logic out in English.
+
+# PROBLEM: a method that returns the sum of two integers
+# CASUAL: Given two integers, calculate the sum
+# PSEUDOCODE: CALCULATE the sum of number1 and number2
+
+# PROBLEM: a method that takes an array of strings, and returns a string that is all those strings concatenated together
+# CASUAL:
+# Given an array of strings
+#
+# Create a new empty string
+#
+# Iterate through the array of strings
+# - append each string to the empty string
+# ***
+# PSEUDOCODE:
+# START
+#
+# READ string_array
+# SET concatenated_strings to new string
+#
+# FOR each string in string_array
+#   SET concatenated_strings to concatenated_strings plus string
+# ENDFOR
+#
+# END
+
+# PROBLEM: a method that takes an array of integers, and returns a new array with every other element
+# CASUAL:
+# Given an array of integers
+#
+# Create an empty array called every_second_integer
+#
+# Iterate through integers with index
+# - If index is odd
+#   - Append the current integer to every_second_integer
+# ***
+# PSEUDOCODE:
+# START
+#
+# READ array of integers into integers
+# SET every_second_integer to an empty array
+#
+# FOR each (integer, index) in integers
+#   IF index is odd
+#     APPEND integer to every_second_integer
+#   ENDIF
+# ENDFOR
+#
+# END
