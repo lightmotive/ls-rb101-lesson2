d7b7ca7a53377b2a5a8432704523be7f7431342a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-03 21:46:48 +0000

    Use strip instead of chomp for user choice
    - Automatically avoids common input errors

--- rock_paper_scissors/main.rb
+++ rock_paper_scissors/main.rb
@@ -35,7 +35,7 @@ loop do
   player1_choice = nil
   loop do
     prompt("Choose one: #{choices_as_strings.join(', ')}")
-    player1_choice = Kernel.gets.chomp.downcase
+    player1_choice = Kernel.gets.strip.downcase
     break if choice_valid?(player1_choice)
 
     prompt("That wasn't a valid choice. Please try again.")
