9069bc12239550aa290d004102b5d40cebaa483a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-03 21:35:39 +0000

    Rename players
    - For now, player1 is user and player2 is computer.
    - The game could expand to multiple human players (local or online).

--- rock_paper_scissors/main.rb
+++ rock_paper_scissors/main.rb
@@ -18,9 +18,9 @@ def choice_valid?(choice)
   choices_as_strings.include?(choice)
 end
 
-def game_result(player_choice, computer_choice)
-  return :tie if player_choice == computer_choice
-  return :win if CHOICES[player_choice.to_sym][:beats].include?(computer_choice.to_sym)
+def game_result(player1_choice, player2_choice)
+  return :tie if player1_choice == player2_choice
+  return :win if CHOICES[player1_choice.to_sym][:beats].include?(player2_choice.to_sym)
 
   :lose
 end
@@ -32,19 +32,19 @@ def game_result_print(result)
 end
 
 loop do
-  user_choice = nil
+  player1_choice = nil
   loop do
     prompt("Choose one: #{choices_as_strings.join(', ')}")
-    user_choice = Kernel.gets.chomp
-    break if choice_valid?(user_choice)
+    player1_choice = Kernel.gets.chomp
+    break if choice_valid?(player1_choice)
 
     prompt("That wasn't a valid choice. Please try again.")
   end
 
-  computer_choice = choices_as_strings.sample
-  prompt("Computer: #{computer_choice}")
+  player2_choice = choices_as_strings.sample
+  prompt("Computer: #{player2_choice}")
 
-  game_result_print(game_result(user_choice, computer_choice))
+  game_result_print(game_result(player1_choice, player2_choice))
 
   puts("\n")
   prompt('Would you like to play again?')
