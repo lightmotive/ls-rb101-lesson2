8a482716818b219cd2ee1435eb4086bf7d7aa308
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-15 22:17:51 +0000

    Complete Lesson 3/Medium 1/Question 7

--- lesson3_practice_problems/practice_problems/03_medium1/questions.rb
+++ lesson3_practice_problems/practice_problems/03_medium1/questions.rb
@@ -99,7 +99,30 @@ p answer - 8
 
 # ***
 puts "\n* Question 7 *"
-# ...
+# One day Spot was playing with the Munster family's home computer and he wrote a small program to mess with their
+# demographic data:
+munsters = {
+  'Herman' => { 'age' => 32, 'gender' => 'male' },
+  'Lily' => { 'age' => 30, 'gender' => 'female' },
+  'Grandpa' => { 'age' => 402, 'gender' => 'male' },
+  'Eddie' => { 'age' => 10, 'gender' => 'male' },
+  'Marilyn' => { 'age' => 23, 'gender' => 'female' }
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member['age'] += 42
+    family_member['gender'] = 'other'
+  end
+end
+
+# After writing that method, he typed the following...and before Grandpa could stop him,
+# he hit the Enter key with his tail:
+mess_with_demographics(munsters)
+
+# Did the family's data get ransacked? Why or why not?
+#   Yes! mess_with_demographics loops through each value, a Hash object, and uses indexed assignment, which is mutating.
+p munsters
 
 # ***
 puts "\n* Question 8 *"
