7d5f6ec32aa9a583b43e2a6d4caab828e6872150
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-15 21:07:00 +0000

    Fix questions output formatting
    - Use puts "\n..." instead of two lines

--- lesson3_practice_problems/practice_problems/03_medium1/questions.rb
+++ lesson3_practice_problems/practice_problems/03_medium1/questions.rb
@@ -1,48 +1,43 @@
 # frozen_string_literal: true
 
 # ***
-puts '
-* Question 1 *'
+puts "\n* Question 1 *"
 # Write a one-line program that prints "The Flintstones Rock!" 10 times with each subsequent line
 # indented 1 space to the right:
 10.times { |idx| puts "#{' ' * idx}The Flintstones Rock!" }
 
 # ***
-puts '
-* Question 2 *'
+puts "\n* Question 2 *"
 # ...
 
 # ***
-puts '
-* Question 3 *'
+puts "\n* Question 3 *"
 # ...
 
 # ***
-puts '
-* Question 4 *'
+puts "\n* Question 4 *"
 # ...
 
 # ***
-puts '
-* Question 5 *'
+puts "\n* Question 5 *"
 # ...
 
 # ***
-puts '
-* Question 6 *'
+puts "\n* Question 6 *"
 # ...
 
 # ***
-puts '
-* Question 7 *'
+puts "\n* Question 7 *"
 # ...
 
 # ***
-puts '
-* Question 8 *'
+puts "\n* Question 8 *"
 # ...
 
 # ***
-puts '
-* Question 9 *'
+puts "\n* Question 9 *"
+# ...
+
+# ***
+puts "\n* Question 10 *"
 # ...
