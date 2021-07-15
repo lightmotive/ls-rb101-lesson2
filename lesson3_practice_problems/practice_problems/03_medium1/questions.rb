47a0a9968c1156d3eade52eedefa09816cc275ca
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-15 20:10:13 +0000

    Initialize Lesson 3/Medium 1 questions

---
+++ lesson3_practice_problems/practice_problems/03_medium1/questions.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# ***
+puts '
+* Question 1 *'
+# ...
+
+# ***
+puts '
+* Question 2 *'
+# ...
+
+# ***
+puts '
+* Question 3 *'
+# ...
+
+# ***
+puts '
+* Question 4 *'
+# ...
+
+# ***
+puts '
+* Question 5 *'
+# ...
+
+# ***
+puts '
+* Question 6 *'
+# ...
+
+# ***
+puts '
+* Question 7 *'
+# ...
+
+# ***
+puts '
+* Question 8 *'
+# ...
+
+# ***
+puts '
+* Question 9 *'
+# ...
