3020a5df990710beef68110c35c265bb0115aa64
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-15 17:04:35 +0000

    Complete Lesson 3/Easy 2/Question 2

--- lesson3_practice_problems/practice_problems/02_easy2/questions.rb
+++ lesson3_practice_problems/practice_problems/02_easy2/questions.rb
@@ -12,7 +12,17 @@ p ages.member?('Spot')
 
 # ***
 puts '* Question 2 *'
-# ...
+
+munsters_description = 'The Munsters are creepy in a good way.'
+# Convert the string in the following ways (code will be executed on original munsters_description above):
+# "tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+p munsters_description.swapcase
+# "The munsters are creepy in a good way."
+p munsters_description.capitalize
+# "the munsters are creepy in a good way."
+p munsters_description.downcase
+# "THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+p munsters_description.upcase
 
 # ***
 puts '* Question 3 *'
