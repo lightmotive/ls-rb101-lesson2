449ebd99b3a30d306dc0d7faf29d3135b66b0065
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-15 16:54:53 +0000

    Complete Lesson 3/Easy 2/Question 1

--- lesson3_practice_problems/practice_problems/02_easy2/questions.rb
+++ lesson3_practice_problems/practice_problems/02_easy2/questions.rb
@@ -2,7 +2,13 @@
 
 # ***
 puts '* Question 1 *'
-# ...
+
+ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 402, 'Eddie' => 10 }
+# Check if "Spot" is present:
+p ages.include?('Spot')
+# Bonus: What are two other hash methods that would work just as well for this solution?
+p ages.key?('Spot')
+p ages.member?('Spot')
 
 # ***
 puts '* Question 2 *'
