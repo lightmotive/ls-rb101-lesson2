62e16955989e752c9885953ec999670e61f4224a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-15 16:54:01 +0000

    Improve shell script output
    - Re-initialize questions

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 
 ## Helpful shell scripts
 - Create Questions file with specified number of questions:
-  
   ```ruby
   touch 'questions.rb'
   printf "# frozen_string_literal: true" > "questions.rb"
-  for i in {1..10}; do printf "\n\n# ***\n# Question $i\nputs '* Question $i *'\n# " >> "questions.rb"; done
+  for i in {1..10}; do printf "\n\n# ***\nputs '* Question $i *'\n# ..." >> "questions.rb"; done
   ```
-  
--- lesson3_practice_problems/practice_problems/02_easy2/questions.rb
+++ lesson3_practice_problems/practice_problems/02_easy2/questions.rb
@@ -1,41 +1,41 @@
 # frozen_string_literal: true
 
 # ***
-# Question 1
-#
+puts '* Question 1 *'
+# ...
 
 # ***
-# Question 2
-#
+puts '* Question 2 *'
+# ...
 
 # ***
-# Question 3
-#
+puts '* Question 3 *'
+# ...
 
 # ***
-# Question 4
-#
+puts '* Question 4 *'
+# ...
 
 # ***
-# Question 5
-#
+puts '* Question 5 *'
+# ...
 
 # ***
-# Question 6
-#
+puts '* Question 6 *'
+# ...
 
 # ***
-# Question 7
-#
+puts '* Question 7 *'
+# ...
 
 # ***
-# Question 8
-#
+puts '* Question 8 *'
+# ...
 
 # ***
-# Question 9
-#
+puts '* Question 9 *'
+# ...
 
 # ***
-# Question 10
-#
+puts '* Question 10 *'
+# ...
