4fb596d1dfbb3f5c7a2c6205b3c6a674d6dbd043
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-16 20:48:02 +0000

    Initialize Lesson 3/Hard 1 questions

---
+++ lesson3_practice_problems/practice_problems/05_hard1/questions.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# ***
+puts "\n* Question 1 *"
+# ...
+
+# ***
+puts "\n* Question 2 *"
+# ...
+
+# ***
+puts "\n* Question 3 *"
+# ...
+
+# ***
+puts "\n* Question 4 *"
+# ...
