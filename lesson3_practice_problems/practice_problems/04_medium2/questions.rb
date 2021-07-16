63e6e1132364e3bfb2a99a19cb02914227847707
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-16 19:12:25 +0000

    Complete Lesson 3/Medium 2/Question 6

--- lesson3_practice_problems/practice_problems/04_medium2/questions.rb
+++ lesson3_practice_problems/practice_problems/04_medium2/questions.rb
@@ -166,4 +166,16 @@ puts "My array looks like this now: #{my_array}"    # ...now: ["pumpkins", "ruta
 
 # ***
 puts "\n* Question 6 *"
-# ...
+# How could the following method be simplified without changing its return value?
+
+# def color_valid(color)
+#   if color == "blue" || color == "green"
+#     true
+#   else
+#     false
+#   end
+# end
+
+def color_valid(color)
+  %w[blue green].include?(color)
+end
