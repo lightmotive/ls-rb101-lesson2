6a60580e2bc5ed39a1eb7814b2a9aeaa96cebe02
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-16 21:14:48 +0000

    Complete Lesson 3/Hard 1/Question 1

--- lesson3_practice_problems/practice_problems/05_hard1/questions.rb
+++ lesson3_practice_problems/practice_problems/05_hard1/questions.rb
@@ -2,7 +2,27 @@
 
 # ***
 puts "\n* Question 1 *"
-# ...
+# What do you expect to happen when the greeting variable is referenced in the last line of the code below?
+
+# rubocop:disable Style/IfUnlessModifier
+if false
+  greeting = 'hello world'
+end
+# rubocop:enable Style/IfUnlessModifier
+
+p greeting
+# => nil
+
+# Control statements don't create new scopes, so variables initialized within are accessible outside.
+# Ruby initializes the value to nil, and since the control expression condition is always false, greeting is never set.
+# The same behavior applies to all control expressions, e.g.:
+# rubocop:disable Style/WhileUntilModifier
+while false
+  greeting2 = 'false'
+end
+# rubocop:enable Style/WhileUntilModifier
+
+p greeting2
 
 # ***
 puts "\n* Question 2 *"
