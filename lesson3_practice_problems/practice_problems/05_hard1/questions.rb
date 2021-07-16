9b7a013183876eb9427a9122e42db75b15db4853
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-16 21:29:24 +0000

    Complete Lesson 3/Hard 1/Question 2

--- lesson3_practice_problems/practice_problems/05_hard1/questions.rb
+++ lesson3_practice_problems/practice_problems/05_hard1/questions.rb
@@ -26,7 +26,16 @@ p greeting2
 
 # ***
 puts "\n* Question 2 *"
-# ...
+# What is the result of the last line in the code below?
+
+greetings = { a: 'hi' }
+informal_greeting = greetings[:a]
+informal_greeting << ' there'
+
+puts informal_greeting #  => "hi there"
+puts greetings # => { :a => 'hi there' }
+# Reason: String#<< mutates the object to which informal_greeting points, which is the same string object stored
+# in greetings[:a].
 
 # ***
 puts "\n* Question 3 *"
