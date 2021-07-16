8679c58da39cffd676c9ec18b47659202c57145e
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-07-16 21:53:38 +0000

    Complete Lesson 3/Hard 1/Question 4

--- lesson3_practice_problems/practice_problems/05_hard1/questions.rb
+++ lesson3_practice_problems/practice_problems/05_hard1/questions.rb
@@ -101,4 +101,34 @@ puts "\n* Question 3 *"
 
 # ***
 puts "\n* Question 4 *"
-# ...
+# Ben was tasked to write a simple ruby method to determine if an input string is an IP address representing
+# dot-separated numbers. e.g. "10.4.5.11". He is not familiar with regular expressions. Alyssa supplied Ben
+# with a method called is_an_ip_number? that determines if a string is a numeric string between 0 and 255 as
+# required for IP numbers and asked Ben to use it.
+
+# def dot_separated_ip_address?(input_string)
+#   dot_separated_words = input_string.split(".")
+#   while dot_separated_words.size > 0 do
+#     word = dot_separated_words.pop
+#     break unless is_an_ip_number?(word)
+#   end
+#   return true
+# end
+
+# Alyssa reviewed Ben's code and says "It's a good start, but you missed a few things. You're not returning
+# a false condition, and you're not handling the case that there are more or fewer than 4 components to the
+# IP address (e.g. "4.5.5" or "1.2.3.4.5" should be invalid)."
+#
+# Help Ben fix his code.
+
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split('.')
+  return false if dot_separated_words.length != 4
+
+  # rubocop:disable Style/WhileUntilModifier
+  while dot_separated_words.size.positive?
+    return false unless is_an_ip_number?(dot_separated_words.pop)
+  end
+  # rubocop:enable Style/WhileUntilModifier
+  true
+end
