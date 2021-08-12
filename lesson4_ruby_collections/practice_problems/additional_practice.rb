f623375c710a4689a1b262f8255e68623afb912c
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-12 00:01:41 +0000

    Apply standardized questions template

--- lesson4_ruby_collections/practice_problems/additional_practice.rb
+++ lesson4_ruby_collections/practice_problems/additional_practice.rb
@@ -1,8 +1,46 @@
 # frozen_string_literal: true
 
+# ***
+puts "\n* Problem 1 *"
 flintstones = %w[Fred Barney Wilma Betty Pebbles BamBam]
 # Turn this array into a hash where the names are the keys and the values are the positions in the array.
 
 flintstones_hash = {}
 flintstones.each_with_index { |name, idx| flintstones_hash[name] = idx }
 p flintstones_hash
+
+# ***
+puts "\n* Problem 2 *"
+# ...
+
+# ***
+puts "\n* Problem 3 *"
+# ...
+
+# ***
+puts "\n* Problem 4 *"
+# ...
+
+# ***
+puts "\n* Problem 5 *"
+# ...
+
+# ***
+puts "\n* Problem 6 *"
+# ...
+
+# ***
+puts "\n* Problem 7 *"
+# ...
+
+# ***
+puts "\n* Problem 8 *"
+# ...
+
+# ***
+puts "\n* Problem 9 *"
+# ...
+
+# ***
+puts "\n* Problem 10 *"
+# ...
