fffe6e7983e6a77cd9ded5e48abe5e0663e7a134
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-12 00:04:04 +0000

    Complete Additional Practice problem 2

--- lesson4_ruby_collections/practice_problems/additional_practice.rb
+++ lesson4_ruby_collections/practice_problems/additional_practice.rb
@@ -11,7 +11,9 @@ p flintstones_hash
 
 # ***
 puts "\n* Problem 2 *"
-# ...
+# Add up all of the ages from the Munster family hash:
+ages = { 'Herman' => 32, 'Lily' => 30, 'Grandpa' => 5843, 'Eddie' => 10, 'Marilyn' => 22, 'Spot' => 237 }
+p ages.values.sum
 
 # ***
 puts "\n* Problem 3 *"
