3b5d294acebef3c40e4c88d14559a85409f0b759
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-12 00:26:17 +0000

    Complete Additional Practice problem 5

--- lesson4_ruby_collections/practice_problems/additional_practice.rb
+++ lesson4_ruby_collections/practice_problems/additional_practice.rb
@@ -29,7 +29,9 @@ p ages.values.min
 
 # ***
 puts "\n* Problem 5 *"
-# ...
+# Find the index of the first name that starts with "Be"
+flintstones = %w[Fred Barney Wilma Betty BamBam Pebbles]
+p(flintstones.index { |name| name.start_with?('Be') })
 
 # ***
 puts "\n* Problem 6 *"
