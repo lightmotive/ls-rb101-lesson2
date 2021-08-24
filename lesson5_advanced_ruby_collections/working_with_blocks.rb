fbcef940a8750cd4513e7248b6f65a9dee3204b0
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-24 23:02:20 +0000

    Complete Working with Blocks example 4

--- lesson5_advanced_ruby_collections/working_with_blocks.rb
+++ lesson5_advanced_ruby_collections/working_with_blocks.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Example 3
+# =========
 [[1, 2], [3, 4]].map do |arr|
   puts arr.first
   arr.first
@@ -13,3 +14,27 @@ end
 #     2. returns the first element of each array (1, then 3).
 #         - map inserts each return value into an array (new array to start).
 # 3. map returns the new array that it built from the values returned from the block passed to map.
+
+# Example 4
+# =========
+my_arr = [[18, 7], [3, 12]].each do |arr|
+  arr.each do |num|
+    puts num if num > 5
+  end
+end
+
+# Detailed breakdown:
+# 1. my_arr is assigned to the result of an Array#each method call.
+# 2. The array calls each and passes a block, which iterates through each element of that array.
+#     - Each element is an array.
+#       - Each array then calls each, passing a block that iterates over each element.
+#         - Each element is a number.
+#         - if the number is > 5, then puts num.
+#           - STDOUT will display 18, then 7, then 12 (each on a separate line).
+#         - The block always returns nil because puts returns nil and there are no statements after the if expression.
+#       - The Array#each returns nil, which isn't used.
+#     - Each returns the original array.
+# 3. The result of the Array#each call is assigned to my_arr, which is [[18, 7], [3, 12]]
+#     - Array#each always returns the caller.
+# 4. The final result is the original array assigned to my_arr
+#     => [[18, 7], [3, 12]]
