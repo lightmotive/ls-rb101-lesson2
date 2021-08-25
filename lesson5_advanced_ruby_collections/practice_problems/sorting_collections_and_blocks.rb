929a09c0bbe480474ca6885ba7b0335b2ca2afaf
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-25 21:09:06 +0000

    Complete Practice Problem 2

--- lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
+++ lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
@@ -6,12 +6,23 @@ puts "\n* Practice Problem 1 *"
 arr = %w[10 11 9 7 8]
 
 p arr.sort_by(&:to_i).reverse
-# Enumerable#sort_by is more efficient than #sort with conversion because it results in fewer String#to_i calls.
+# Enumerable#sort_by is more efficient than #sort with conversion because sort_by requires in fewer String#to_i calls.
 # https://docs.ruby-lang.org/en/master/Enumerable.html#:~:text=this%20is%20exactly%20what%20sort_by%20does%20internally.
 
 # ***
 puts "\n* Practice Problem 2 *"
-# ...
+# How would you order this array of hashes based on the year of publication of each book, from the earliest
+# to the latest?
+books = [
+  { title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967' },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925' },
+  { title: 'War and Peace', author: 'Leo Tolstoy', published: '1869' },
+  { title: 'Ulysses', author: 'James Joyce', published: '1922' }
+]
+
+p(books.sort_by { |hash| hash[:published].to_i })
+# While it's not necessary to convert the publish year to integer when looking at the sample data, we'll convert
+# in case the data ever would contain a pre-1000 year.
 
 # ***
 puts "\n* Practice Problem 3 *"
