bd1fa4245194670980fd67c11e687ca15b0a35d2
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-25 20:54:20 +0000

    Set up Practice Problems
    - Sorting, Nested Collections, and Working with Blocks

---
+++ lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+# ***
+puts "\n* Practice Problem 1 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 2 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 3 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 4 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 5 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 6 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 7 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 8 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 9 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 10 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 11 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 12 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 13 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 14 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 15 *"
+# ...
+
+# ***
+puts "\n* Practice Problem 16 *"
+# ...
