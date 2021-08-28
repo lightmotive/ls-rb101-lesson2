908baa85dabf19936d7ed4bff8acc7a8158ca2ed
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-28 21:58:37 +0000

    Complete Practice Problem 12

--- lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
+++ lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
@@ -131,7 +131,13 @@ p multiples_of_three
 
 # ***
 puts "\n* Practice Problem 12 *"
-# ...
+# Given the following data structure, and without using the Array#to_h method, write some code that will return a hash
+# where the key is the first item in each sub array and the value is the second item.
+arr = [[:a, 1], %w[b two], ['sea', { c: 3 }], [{ a: 1, b: 2, c: 3, d: 4 }, 'D']]
+# expected return value: {:a=>1, "b"=>"two", "sea"=>{:c=>3}, {:a=>1, :b=>2, :c=>3, :d=>4}=>"D"}
+
+hsh = arr.each_with_object({}) { |sub_arr, hsh| hsh[sub_arr[0]] = sub_arr[1] }
+p hsh
 
 # ***
 puts "\n* Practice Problem 13 *"
