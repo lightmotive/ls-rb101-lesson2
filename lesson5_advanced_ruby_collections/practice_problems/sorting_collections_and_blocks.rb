5ae10c3ccc7e37c9d4728c7e26fa894544fed428
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-28 21:45:57 +0000

    Complete Practice Problem 10

--- lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
+++ lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
@@ -113,6 +113,11 @@ p new_arr
 
 # ***
 puts "\n* Practice Problem 10 *"
+# Given the following data structure and without modifying the original array, use the map method to return a new array
+# identical in structure to the original but where the value of each integer is incremented by 1.
+arr_hsh = [{ a: 1 }, { b: 2, c: 3 }, { d: 4, e: 5, f: 6 }]
+arr_hsh2 = arr_hsh.map { |hsh| hsh.transform_values { |value| value + 1 } }
+p arr_hsh2
 
 # ***
 puts "\n* Practice Problem 11 *"
