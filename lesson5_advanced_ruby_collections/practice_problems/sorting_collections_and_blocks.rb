0872ef009fab960fb6a5028d1aa75708605488d6
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-28 22:06:36 +0000

    Complete Practice Problem 13

--- lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
+++ lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
@@ -141,7 +141,12 @@ p hsh
 
 # ***
 puts "\n* Practice Problem 13 *"
-# ...
+# Given the following data structure, return a new array containing the same sub-arrays as the original but ordered
+# logically by only taking into consideration the odd numbers they contain.
+arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
+# Expected result: [[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+
+p(arr.sort_by { |sub_arr| sub_arr.select(&:odd?) })
 
 # ***
 puts "\n* Practice Problem 14 *"
