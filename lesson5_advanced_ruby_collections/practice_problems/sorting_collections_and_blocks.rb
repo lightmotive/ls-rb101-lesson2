e11ed54ba89469aae60f0731e8ad545936fe115e
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-28 22:41:45 +0000

    Complete Practice Problem 15

--- lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
+++ lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
@@ -169,7 +169,20 @@ end)
 
 # ***
 puts "\n* Practice Problem 15 *"
-# ...
+# Given this data structure write some code to return an array which contains only the hashes where all the integers
+# are even.
+arr = [{ a: [1, 2, 3] }, { b: [2, 4, 6], c: [3, 6], d: [4] }, { e: [8], f: [6, 10] }]
+
+# Basic logic:
+# Iterate through each item in the array (hash)
+#   Iterate through each value of the hash (arrays of numbers)
+#     Iterate through each number; if any is odd, exclude the entire hash.
+# Ruby has methods for succinct and algorithmically efficient code.
+#   any?(&:odd) will be more efficient than all?(&:even) because the former does not need to check all array values.
+
+p(arr.reject do |hsh|
+  hsh.values.map { |numbers| numbers.any?(&:odd?) }.any?(true)
+end)
 
 # ***
 puts "\n* Practice Problem 16 *"
