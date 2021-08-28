a78210af1d4d699db149f99241878f3bd69dbf11
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-08-28 21:52:44 +0000

    Complete Practice Problem 11

--- lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
+++ lesson5_advanced_ruby_collections/practice_problems/sorting_collections_and_blocks.rb
@@ -116,12 +116,18 @@ puts "\n* Practice Problem 10 *"
 # Given the following data structure and without modifying the original array, use the map method to return a new array
 # identical in structure to the original but where the value of each integer is incremented by 1.
 arr_hsh = [{ a: 1 }, { b: 2, c: 3 }, { d: 4, e: 5, f: 6 }]
+
 arr_hsh2 = arr_hsh.map { |hsh| hsh.transform_values { |value| value + 1 } }
 p arr_hsh2
 
 # ***
 puts "\n* Practice Problem 11 *"
-# ...
+# Given the following data structure use a combination of methods, including either the select or reject method, to
+# return a new array identical in structure to the original but containing only the integers that are multiples of 3.
+arr = [[2], [3, 5, 7], [9], [11, 13, 15]]
+
+multiples_of_three = arr.map { |e| e.select { |n| (n % 3).zero? } }
+p multiples_of_three
 
 # ***
 puts "\n* Practice Problem 12 *"
