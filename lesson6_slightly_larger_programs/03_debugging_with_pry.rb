95ba1f782f717759abe0625fe790dd14efb27e9a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-24 18:38:35 +0000

    Learn and use pry-byebug

--- lesson6_slightly_larger_programs/03_debugging_with_pry.rb
+++ lesson6_slightly_larger_programs/03_debugging_with_pry.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
 require 'pry'
+require 'pry-byebug'
 
-def test
-  a = 1
-  puts a
+def fizzbuzz_value(num)
+  div_by5 = (num % 5).zero?
+  div_by3 = (num % 3).zero?
+
+  binding.pry
+
+  if div_by5 && div_by3 then 'FizzBuzz'
+  elsif div_by5 then 'Buzz'
+  elsif div_by3 then 'Fizz'
+  else num
+  end
 end
 
-arr = [1, 2, 3]
+def fizzbuzz(start_num, end_num)
+  (start_num..end_num).map do |num|
+    fizzbuzz_value(num)
+  end
+end
 
-binding.pry
-p arr
+p(fizzbuzz(1, 15).join(', ') == '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz')
