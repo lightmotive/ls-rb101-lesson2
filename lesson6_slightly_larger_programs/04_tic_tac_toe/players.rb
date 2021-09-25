2e45be0f5fe706a259936c9481652de0a9a39d95
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-25 19:59:47 +0000

    Consolidate computer and user player code
    - Single file: players.rb

--- lesson6_slightly_larger_programs/04_tic_tac_toe/players.rb
+++ lesson6_slightly_larger_programs/04_tic_tac_toe/players.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'board'
 require_relative '../../../ruby-common/prompt'
 
 def player_move_to_coordinates(move)
@@ -16,4 +17,11 @@ def player_move!(mark, board_state)
   )
 
   board_mark!(mark, move_coordinates, board_state)
+end
+
+def computer_move!(mark, board_state)
+  coordinates = available_move_coordinates(board_state).sample
+  # TODO: Add computer play intelligence
+  board_mark!(mark, coordinates, board_state)
+  puts "Computer moved: #{coordinates[:row]},#{coordinates[:column]}"
 end
