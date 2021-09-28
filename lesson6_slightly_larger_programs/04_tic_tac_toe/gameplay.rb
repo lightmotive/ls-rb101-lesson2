73bd771851c09105008894438d7271e972ae2d9a
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-28 22:00:21 +0000

    Move win logic to gameplay_win.rb

--- lesson6_slightly_larger_programs/04_tic_tac_toe/gameplay.rb
+++ lesson6_slightly_larger_programs/04_tic_tac_toe/gameplay.rb
@@ -2,52 +2,7 @@
 
 require_relative 'board_state'
 require_relative 'gameplay_score'
-
-def winning_line_mark(spaces_rows)
-  spaces_rows.each do |spaces|
-    marks = spaces.map { |space| space[:mark] }
-    unique_marks = marks.uniq
-    return unique_marks.first if unique_marks.size == 1 && !space_available?(unique_marks.first)
-  end
-
-  nil
-end
-
-def winning_mark(board_state)
-  winning_mark = winning_line_mark(board_rows(board_state)) # Check rows
-  return winning_mark unless winning_mark.nil?
-
-  winning_mark = winning_line_mark(board_columns(board_state))
-  return winning_mark unless winning_mark.nil?
-
-  winning_mark = winning_line_mark(board_diagonals(board_state))
-  return winning_mark unless winning_mark.nil?
-
-  nil
-end
-
-def player_won(winning_mark, board_state, players)
-  board_display(board_state)
-
-  winner = players.select { |player| player[:mark] == winning_mark }.first
-  no_computer_players = players.count { |player| player[:is_computer] }.zero?
-
-  winner[:score] += 1
-
-  if no_computer_players || winner[:is_computer] then puts "#{winner[:name]} won the game!"
-  else puts "You won the game! (#{winner[:mark]})"
-  end
-end
-
-def end_game_winner?(board_state, players)
-  winning_mark = winning_mark(board_state)
-  unless winning_mark.nil?
-    player_won(winning_mark, board_state, players)
-    return true
-  end
-
-  false
-end
+require_relative 'gameplay_win'
 
 def display_tie(board_state)
   board_display(board_state)
