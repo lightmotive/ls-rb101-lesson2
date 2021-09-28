93728ed5468091311bdc8a1e918f215bfdfa4003
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-28 15:46:50 +0000

    "Move" to "Mark"

--- lesson6_slightly_larger_programs/04_tic_tac_toe/players.rb
+++ lesson6_slightly_larger_programs/04_tic_tac_toe/players.rb
@@ -5,7 +5,7 @@ require_relative '../../../ruby-common/prompt'
 
 def player_move!(player, board_state)
   move_number = prompt_until_valid(
-    "#{player[:name]} (#{player[:mark]}), enter your move number:",
+    "#{player[:name]}, enter the number to mark (#{player[:mark]}):",
     get_input: -> { gets.strip },
     convert_input: ->(input) { input.to_i },
     validate: ->(number) { validate_move(number, board_state) }
@@ -18,5 +18,5 @@ def computer_move!(mark, board_state)
   move_number = available_moves(board_state).sample
   # TODO: Add computer play intelligence
   board_mark!(mark, move_number, board_state)
-  puts "Computer moved: #{move_number}"
+  puts "Computer marked #{move_number}"
 end
