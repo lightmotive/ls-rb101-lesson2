e31f699f41d3a75d5d1af26f67a749f3c32779d9
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-28 17:01:37 +0000

    Move gameplay methods from main.rb to gameplay.rb

--- lesson6_slightly_larger_programs/04_tic_tac_toe/gameplay.rb
+++ lesson6_slightly_larger_programs/04_tic_tac_toe/gameplay.rb
@@ -24,3 +24,78 @@ def winning_mark(board_state)
 
   nil
 end
+
+def display_winner(winning_mark, board_state, players)
+  board_display(board_state)
+
+  winner = players.select { |player| player[:mark] == winning_mark }.first
+  no_computer_players = players.count { |player| player[:is_computer] }.zero?
+
+  if no_computer_players || winner[:is_computer] then puts "#{winner[:name]} won!"
+  else puts "You won! (#{winner[:mark]})"
+  end
+end
+
+def end_game_winner?(board_state, players)
+  winning_mark = winning_mark(board_state)
+  unless winning_mark.nil?
+    display_winner(winning_mark, board_state, players)
+    return true
+  end
+
+  false
+end
+
+def display_tie(board_state)
+  board_display(board_state)
+  puts 'Tie!'
+end
+
+def end_game_tie?(board_state)
+  if board_full?(board_state)
+    display_tie(board_state)
+    return true
+  end
+
+  false
+end
+
+def end_game?(board_state, players)
+  return true if end_game_winner?(board_state, players)
+  return true if end_game_tie?(board_state)
+
+  false
+end
+
+def redraw(board_state, players)
+  board_display(board_state, include_move_values: true)
+  players_display(players)
+  puts
+end
+
+def play_and_end_game?(board_state, players)
+  end_game = false
+
+  players.each do |player|
+    if player[:is_computer] then computer_move!(player[:mark], board_state)
+    else player_move!(player, board_state)
+    end
+
+    redraw(board_state, players)
+
+    end_game = end_game?(board_state, players)
+    break if end_game
+  end
+
+  end_game
+end
+
+def start_game(players)
+  board_state = board_state_create
+  redraw(board_state, players)
+
+  loop do
+    end_game = play_and_end_game?(board_state, players)
+    break if end_game
+  end
+end
