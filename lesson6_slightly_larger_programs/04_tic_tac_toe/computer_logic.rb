56f14547ef303fd90cbe6ad90b8cd1a944c817de
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-30 22:10:46 +0000

    Add computer offense (move to win)

--- lesson6_slightly_larger_programs/04_tic_tac_toe/computer_logic.rb
+++ lesson6_slightly_larger_programs/04_tic_tac_toe/computer_logic.rb
@@ -2,12 +2,24 @@
 
 require_relative 'board_state'
 
+# TODO: Improve computer logic to play best strategy:
+# - Strategy: https://en.wikipedia.org/wiki/Tic-tac-toe#:~:text=cat's%20game%22%5B15%5D)-,strategy
+# - Minimax algorithm: https://www.youtube.com/watch?v=trKjYdBASyQ
+
+def move_to_win(with_mark, board_state)
+  player_completion_square_numbers = board_squares_that_complete_line(with_mark, board_state)
+  player_completion_square_numbers.first
+end
+
 def move_defense(against_mark, board_state)
   opponent_completion_square_numbers = board_squares_that_complete_line(against_mark, board_state)
   opponent_completion_square_numbers.sample
 end
 
 def computer_move_select(mark, opponent_mark, board_state)
+  complete_line_square_number = move_to_win(mark, board_state)
+  return complete_line_square_number unless complete_line_square_number.nil?
+
   defend_square_number = move_defense(opponent_mark, board_state)
   return defend_square_number unless defend_square_number.nil?
 
