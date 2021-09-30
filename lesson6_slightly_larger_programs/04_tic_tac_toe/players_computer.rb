0f0718a1e561e83677baecd4cda9c036ae1a0373
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-09-30 17:44:46 +0000

    Replace hard-coded 'Computer' with constant var

--- lesson6_slightly_larger_programs/04_tic_tac_toe/players_computer.rb
+++ lesson6_slightly_larger_programs/04_tic_tac_toe/players_computer.rb
@@ -6,5 +6,5 @@ def computer_move!(mark, board_state)
   move_number = available_moves(board_state).sample
   # TODO: Add computer play intelligence
   board_mark!(mark, move_number, board_state)
-  puts "Computer marked #{move_number}"
+  puts "#{COMPUTER_NAME} marked #{move_number}"
 end
