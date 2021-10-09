bfe763892fd8970b118519fc9b250448d3cfe849
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-10-09 23:47:19 +0000

    Create Twenty-One flowchart and initial logic

---
+++ lesson6_slightly_larger_programs/08_twenty_one.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+# Flowchart: https://app.terrastruct.com/diagrams/465606234
+
+# P1: Understand the problem
+# With a player and a dealer, randomly deal cards from a standard 52-card deck
+# without exceeding 21. The winner is the player closest to 21.
+
+# P2: Examples/Test Cases
+
+# P3: Data Structure
+# ** Cards **
+# Array of hashes; one hash for each card; hash includes:
+# - name (suit and value), e.g., Ace of Hearts, 10 of Diamonds, King of Clubs.
+# - Integer or value(currentCards) method to determine the card's value based
+#   on current hand.
+
+# P4: Algorithm
+
+# P5: Implementation
