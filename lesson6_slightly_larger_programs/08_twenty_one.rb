1ae697cd14ec50bd9e1f75a16bbf3208a179add3
LightMotive <7719153+lightmotive@users.noreply.github.com>
2021-10-10 00:10:32 +0000

    Update Twenty-One logic
    - Add dealer play rules.
    - Add basic play structure.
    - Move **calculate hand value** to Algorithm phase.
    - Simplify deck data structure.

--- lesson6_slightly_larger_programs/08_twenty_one.rb
+++ lesson6_slightly_larger_programs/08_twenty_one.rb
@@ -3,18 +3,31 @@
 # Flowchart: https://app.terrastruct.com/diagrams/465606234
 
 # P1: Understand the problem
-# With a player and a dealer, randomly deal cards from a standard 52-card deck
-# without exceeding 21. The winner is the player closest to 21.
+# With a player and a dealer, deal cards from a standard 52-card deck.
+# - Start with 2 cards face up for player, and 1 up/1 down for the dealer.
+# Player goes first: they can hit as long as they don't bust (exceed 21),
+# or stay.
+# Dealer goes second if player doesn't bust.
+# - Dealer (computer) play rules:
+#   - Hit until value is >= 17.
+# The winner is the player closest to 21, or the one who doesn't bust.
 
 # P2: Examples/Test Cases
+# N/A for now.
 
 # P3: Data Structure
 # ** Cards **
 # Array of hashes; one hash for each card; hash includes:
-# - name (suit and value), e.g., Ace of Hearts, 10 of Diamonds, King of Clubs.
-# - Integer or value(currentCards) method to determine the card's value based
-#   on current hand.
+# - suit (hearts, diamonds, clubs, spades)
+# - value (2-10, j, q, k, ace)
 
 # P4: Algorithm
+# ** Calculate hand value **
+# Sum all values as follows:
+# - 2-10: face value
+# - Jack, Queen, King: 10
+# - Ace: depends on hand value...
+#   - If hand value is <= 21 when ace is 11, then ace is 11.
+#   - Otherwise, aces are worth 1.
 
 # P5: Implementation
